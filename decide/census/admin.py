c6af36e674e7e1b1c29cc68d53cd220a6f6fb22c
Jose Cobo <cobogue@gmail.com>
2020-11-19 20:32:43 +0100

    initialising

---
+++ decide/census/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from .models import Census
+
+
+class CensusAdmin(admin.ModelAdmin):
+    list_display = ('voting_id', 'voter_id', 'adscription')
+    list_filter = ('voting_id', 'adscription')
+    search_fields = ('voter_id','adscription')
+
+
+admin.site.register(Census, CensusAdmin)
---
+++ decide/census/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Census(models.Model):
+    voting_id = models.PositiveIntegerField()
+    voter_id = models.PositiveIntegerField()
+    
+    #Practica 1 - Tarea:
+    adscription = models.CharField(max_length=50, default='')
+
+    class Meta:
+        unique_together = (('voting_id', 'voter_id'),)
