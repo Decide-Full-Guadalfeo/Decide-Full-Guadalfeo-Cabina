9ec8fcc942b0faad905c30b4315bd62c9cf12fda
Jose Cobo <cobogue@gmail.com>
2020-12-10 11:40:21 +0100

    Heroku

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -165,6 +165,9 @@ KEYBITS = 256
 ALLOWED_VERSIONS = ['v1', 'v2']
 DEFAULT_VERSION = 'v1'
 
+BASEURL="https://decide-guadalfeo-cabina.herokuapp.com"
+APIS = {}
+
 try:
     from local_settings import *
 except ImportError:
@@ -180,3 +183,7 @@ if os.path.exists("config.jsonnet"):
 
 
 INSTALLED_APPS = INSTALLED_APPS + MODULES
+
+import django_heroku
+
+django_heroku.settings(locals())
