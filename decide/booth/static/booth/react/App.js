eeeb4cda47aec56e0e5a14eebc243c23ed9ec3f0
Jose Cobo <cobogue@gmail.com>
2020-12-23 21:10:15 +0100

    Autentificacion react

--- decide/booth/static/booth/react/App.js
+++ decide/booth/static/booth/react/App.js
@@ -1,24 +1,78 @@
 'use strict';
 
-import Test from "./Test";
-import Test2 from "./Test2";
-const {useState} = React;
+import Login from "./Login";
+const { useState, useEffect } = React;
 
 const App = () => {
-  
-const [t1, setT1] = useState(true)
 
+  /*############### STATE ###############*/
+  const [token, setToken] = useState(null)
+  const [user, setUser] = useState(null)
+
+
+  /*############### UTILITY FUNCTIONS ###############*/
+  const post = (url, data) => {
+    // Default options are marked with *
+    var fdata = {
+      body: JSON.stringify(data),
+      headers: {
+        'content-type': 'application/json',
+      },
+      method: 'POST',
+    };
+
+    if (token) {
+      fdata.headers['Authorization'] = 'Token ' + token;
+    }
+
+    return fetch(url, fdata)
+      .then(response => {
+        if (response.status === 200) {
+          return response.json();
+        } else {
+          return Promise.reject(response.statusText);
+        }
+      });
+  }
+
+  const logout = () => {
+    const data = { token };
+    post("/gateway/authentication/logout/", data);
+    setToken(null);
+    setUser(null);
+    document.cookie = 'decide=;';
+  }
+
+
+  /*############### FUNCTIONALITY ###############*/
+  console.log(user)
+
+  const view = () => {
+    if(user) {
+      return(
+        <div>
+          <p>Hola {user.username}</p>
+          <button onClick={logout}>Logout</button>
+        </div>
+      )
+    }
+    else{
+      return(
+        <Login post={post} setToken={setToken} setUser={setUser} />
+      )
+    }
+  }
+
+  /*############### RETURN ###############*/
   return (
     <div className="App">
-        <p>This is the root</p>
-        <button type="button" className="btn btn-primary" onClick={() => setT1(!t1)}>
-            SwitchTest
-        </button>
-        {t1 ? <Test/> : <Test2/>}
+      
+      {view()}
+
     </div>
   );
 
 }
 
 const domContainer = document.querySelector('#react-root');
-ReactDOM.render(<App/>, domContainer);
+ReactDOM.render(<App />, domContainer);
---
+++ decide/booth/static/booth/react/Login.js
@@ -0,0 +1,86 @@
+'use strict';
+const { useState, useEffect } = React
+
+const Login = ({ post, setToken, setUser }) => {
+
+    /*############### STATE ###############*/
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+
+    /*############### UTILITY FUNCTIONS ###############*/
+    const getUser = (tkn) => {
+        var data = { token: tkn };
+        post("/gateway/authentication/getuser/", data)
+            .then(data => {
+                setUser(data);
+            }).catch(error => {
+                console.log(error)//this.showAlert("danger", '{% trans "Error: " %}' + error);
+            });
+    }
+
+    const init = () => {
+        const cookies = document.cookie.split("; ");
+
+        cookies.forEach((c) => {
+            var cs = c.split("=");
+            if (cs[0] == 'decide' && cs[1]) {
+                setToken(cs[1]);
+                getUser(cs[1]);
+            }
+        });
+
+    }
+
+    const onSubmitLogin = (event) => {
+        event.preventDefault();
+
+        const form = { username, password };
+
+        post("/gateway/authentication/login/", form)
+            .then(data => {
+                document.cookie = 'decide=' + data.token + ';';
+                setToken(data.token);
+                getUser(data.token);
+            })
+            .catch(error => {
+                console.log(error)//this.showAlert("danger", '{% trans "Error: " %}' + error);
+            });
+    }
+
+
+    /*############### FUNCTIONALITY ###############*/
+
+    useEffect(() => {
+        init()
+    }, [])
+
+
+    /*############### RETURN ###############*/
+    return (
+        <form onSubmit={onSubmitLogin}>
+            <label>Username</label>
+            <input
+                type="text"
+                value={username}
+                onChange={e => setUsername(e.target.value)}
+                autoComplete="username"
+                required
+            />
+
+            <label>Password</label>
+            <input
+                type="password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                autoComplete="current-password"
+                required
+            />
+
+            <button type="">Login</button>
+
+        </form>
+    );
+}
+export default Login;
