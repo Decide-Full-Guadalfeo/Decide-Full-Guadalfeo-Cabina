a34ab8bf4b40e7d9784273c59936eaaca173294a
FranQuintela <fraquivel@alum.us.es>
2021-01-05 21:09:21 +0100

    merged and mig files included

--- decide/booth/static/booth/react/Voting/Voting.js
+++ decide/booth/static/booth/react/Voting/Voting.js
@@ -1,8 +1,8 @@
 "use strict";
 const { useState } = React;
 
-
 const Voting = ({ utils, value }) => {
+  /*############### STATE ###############*/
 
   const [selectedAnswer, setSelectedAnswer] = useState(null);
 
@@ -13,186 +13,116 @@ const Voting = ({ utils, value }) => {
     y: BigInt.fromJSONObject(voting.pub_key.y.toString()),
   };
 
-  const encrypt = () => {
-    const bigmsg = BigInt.fromJSONObject(selectedAnswer);
+  const encrypt = (options) => {
+    const bigmsg = BigInt.fromJSONObject(options.toString());
     const cipher = ElGamal.encrypt(bigpk, bigmsg);
     return cipher;
   };
-  console.log(selectedAnswer);
+
   const sendVoting = (event) => {
     event.preventDefault();
 
-    if (selectedAnswer == null) {
-      utils.setAlert({ lvl: "error", msg: "Please select an option" });
-    } else {
-      const v = encrypt();
-      const data = {
-        vote: { a: v.alpha.toString(), b: v.beta.toString() },
-        voting: voting.id,
-        voter: utils.user.id,
-        token: utils.token,
-      };
-      utils
-        .post("/gateway/store/", data)
-        .then((data) => {
-          utils.setAlert({
-            lvl: "success",
-            msg: "Conglatulations. Your vote has been sent",
-          });
-        })
-        .catch((error) => {
-          utils.setAlert({ lvl: "danger", msg: "Error: " + error });
+    const options = getInput();
+
+    const v = encrypt(options);
+    const data = {
+      vote: { a: v.alpha.toString(), b: v.beta.toString() },
+      voting: voting.id,
+      voter: value.user_id,
+      token: value.token,
+    };
+    console.log(data);
+    utils
+      .post("/gateway/store/", data)
+      .then((data) => {
+        utils.setAlert({
+          lvl: "success",
+          msg: "Conglatulations. Your vote has been sent",
         });
+      })
+      .catch((error) => {
+        utils.setAlert({ lvl: "danger", msg: "Error: " + error });
+      });
+  };
+
+  const getInput = (event) => {
+    let res = {};
+    let a = document.getElementsByClassName("question");
+    for (let i = 0; i < a.length; i++) {
+      const titulo = a[i].children[0].innerHTML;
+      let inputs = a[i].getElementsByTagName("input");
+      for (let j = 0; j < inputs.length; j++) {
+        if (inputs[j].checked) {
+          res[titulo] = inputs[j].value;
+        }
+      }
     }
+    res["sex"] = value.sex;
+    res["age"] = value.age;
+    res["grade"] = value.grade;
+    res["year"] = value.year;
+    console.log(res);
+    return res;
   };
 
   /*############### FUNCTIONALITY ###############*/
-  // For the flip effect
-  $(".flip-card").click(function () {
-    $(".flip-card.flipped").removeClass("flipped");
-    $(this).addClass("flipped");
+  //   show the first element, the others are hide by default
+  $(document).ready(function () {
+    $("div.question:first-of-type").addClass("active-question");
+
+    console.log("doc ready");
+    if ($(".question").length) {
+      console.log("Element exists");
+    }else{
+      console.log("Element doesnt exists");
+
+    }
+    var colors = new Array("#EF476F", "#FFD166", "#06D6A0");
+    // new colors = ["#EF476F","#FFD166","#06D6A0","#118AB2","#073B4C"];
+
+    $(".question").each(function (index) {
+      // console.log(index + ": " + $(this).text());
+      console.log(index + ": " + colors[index]);
+
+      $(this).css({
+        "background-color": colors[index],
+        "filter":"brightness(95%)"
+      }); 
+      // console.log(index + ": " + $(this).text());
+    });
+
+    // $( "option" ).each( function(option) {
+    //   console.log('do something with this list item', option);
+    // })
   });
+  // BUTTONS NOT WORKING
+  // $("button#next-question").click(function () {
+  //   console.log("next");
 
-  $(".flip-card.flipped").click(function () {
-    $(this).removeClass("flipped");
-  });
+  //   var active_question = $("div.active-question");
+  //   if (active_question.next().hasClass("question")) {
+  //     active_question.next().addClass("active-question");
+  //     active_question.removeClass("active-question");
+  //   }
+  // });
+  // $("button#prev-question").click(function () {
+  //   console.log("prev");
 
-  // For the smooth scroll effect
-
-  // $(window).scroll(function () {
-  //   $("#candidatura1").fadeOut(); // replace only #btop with your <div id=" ">
-
-  //   // if ($(this).scrollTop() > 100) {
-  //   //   $("#candidatura2").fadeIn(); // replace only #btop with your <div id=" ">
-  //   // } else {
-  //   //   $("#candidatura2").fadeOut(); // replace only #btop with your <div id=" ">
-  //   // }
+  //   var active_question = $("div.active-question");
+  //   if (active_question.prev().hasClass("question")) {
+  //     active_question.prev().addClass("active-question");
+  //     active_question.removeClass("active-question");
+  //   }
+  // });
+  // For the flip effect, not working
+  // $(".flip-card").click(function () {
+  //   console.log("clicked");
+  //   $(".flip-card.flipped").removeClass("flipped");
+  //   $(this).addClass("flipped");
   // });
 
-  // $("#candidatura1").scroll(){
-  //   console.log("Scroll detected")
-  // };
-  // $(window).scroll(function () {
-  //   // $( "span" ).css( "display", "inline" ).fadeOut( "slow" );
-    const encrypt = (options) => {
-        const bigmsg = BigInt.fromJSONObject(options.toString());
-        const cipher = ElGamal.encrypt(bigpk, bigmsg);
-        return cipher;
-    }
-    
-    const sendVoting = (event) => {
-        event.preventDefault();
-
-        const options = getInput()
-
-        const v = encrypt(options);
-        const data = {
-            vote: { a: v.alpha.toString(), b: v.beta.toString() },
-            voting: voting.id,
-            voter: value.user_id,
-            token: value.token,
-        }
-        console.log(data)
-        utils.post("/gateway/store/", data)
-            .then(data => {
-                utils.setAlert({ lvl: 'success', msg: 'Conglatulations. Your vote has been sent' });
-            })
-            .catch(error => {
-                utils.setAlert({ lvl: 'danger', msg: 'Error: ' + error, });
-            });
-    }
-
-    const getInput = (event) => {
-        let res = {}
-        let a = document.getElementsByClassName('question')
-        for (let i = 0; i < a.length; i++) {
-            const titulo = a[i].children[0].innerHTML;
-            let inputs = a[i].getElementsByTagName('input')
-            for (let j = 0; j < inputs.length; j++) {
-                if (inputs[j].checked) {
-                    res[titulo] = inputs[j].value
-                }
-            }
-        }
-        res['sex'] = value.sex
-        res['age'] = value.age
-        res['grade'] = value.grade
-        res['year'] = value.year
-        console.log(res)
-        return res
-    }
-
-  //   console.log("Scrolled" + ($(window).scrollTop() + window.innerHeight));
-  //   // alert("La barra vertical se ha movido " + $(window).scrollTop() + " pixels");
-  //   // $(window).scrollTop()+window.innerHeight;
-  //   $("html, body").animate(
-  //     {
-  //       // scrollTop: $("#candidatura2").offset().top
-  //       scrollTop: $(window).scrollTop() + window.innerHeight,
-  //     },
-  //     2000
-  //   );
-  // });
-
-  // var lastScrollTop = 0;
-  // $(window).scroll(function () {
-  //   // console.log("Scrolled" + ($(window).scrollTop() + window.innerHeight));
-
-
-    /*############### RETURN ###############*/
-    return (
-        <div className="voting">
-
-            <form onSubmit={sendVoting}>
-                {voting.question.map(o => (
-                    <div className='question'>
-                        <h2>{o.desc}</h2>
-                        {o.options.map(p => (
-                            <div>
-                                <input type="radio" name={o.desc} value={p.number} required />
-                                {p.option}
-                                <br />
-                            </div>
-                        ))}
-                    </div>
-                ))}
-                <button>Vote</button>
-            </form>
-
-        </div >
-    );
-}
-export default Voting;
-
-/* onChange={e => setObjeto(...objeto,{[o.desc]:p.number})}
-
-<h2>{voting.question.desc}</h2>
-
-  //   console.log("lastScrollTop" + lastScrollTop);
-  //   console.log("st" + st);
-
-  //   // if (Math.abs(st - lastScrollTop) > umbral) {
-  //     if (st > lastScrollTop) {
-  //       $("html, body").animate(
-  //         {
-  //           scrollTop: $("#candidatura2").offset().top
-  //           // scrollTop: $(window).scrollTop() + window.innerHeight,
-  //         },
-  //         2000
-  //       );
-  //     } else {
-  //       // upscroll code
-  //       $("html, body").animate(
-  //         {
-  //           scrollTop: $("#candidatura1").offset().top
-  //           // scrollTop: $(window).scrollTop() - window.innerHeight,
-  //         },
-  //         2000
-  //       );
-  //     }
-  //   // }
-  //   lastScrollTop = st;
-
+  // $(".flip-card.flipped").click(function () {
+  //   $(this).removeClass("flipped");
   // });
 
   $(function () {
@@ -200,22 +130,38 @@ export default Voting;
       var delta = e.originalEvent.deltaY;
 
       if (delta > 0) {
-        $("html, body").animate(
-          {
-            // scrollTop: $("#candidatura2").offset().top,
-            scrollTop: $(window).scrollTop() + window.innerHeight,
-          },
-          1000
-        );
+        var active_question = $("div.active-question");
+
+        if (active_question.next().hasClass("question")) {
+          active_question.next().addClass("active-question");
+          active_question.removeClass("active-question");
+        }
+        console.log("scrolled downs");
+        // $("html, body").animate(
+        //   {
+        //     // scrollTop: $("#candidatura2").offset().top,
+        //     scrollTop: $(window).scrollTop() + window.innerHeight,
+        //   },
+        //   1000
+        // );
       } else {
+        // $("div.active-question").prev().addClass("active-question");
+        // $("div.active-question").removeClass("active-question");
+        var active_question = $("div.active-question");
+        if (active_question.prev().hasClass("question")) {
+          active_question.prev().addClass("active-question");
+          active_question.removeClass("active-question");
+        }
+        console.log("scrolled up");
+
         // upscroll code
-        $("html, body").animate(
-          {
-            // scrollTop: $("#candidatura1").offset().top,
-            scrollTop: $(window).scrollTop() - window.innerHeight,
-          },
-          1000
-        );
+        // $("html, body").animate(
+        //   {
+        //     // scrollTop: $("#candidatura1").offset().top,
+        //     scrollTop: $(window).scrollTop() - window.innerHeight,
+        //   },
+        //   1000
+        // );
       }
       return false; // this line is only added so the whole page won't scroll in the demo
     });
@@ -223,142 +169,91 @@ export default Voting;
 
   /*############### RETURN ###############*/
   return (
-    <div className="voting">
-      <h2>{voting.question.desc}</h2>
-      {/* CANDIDATURA 1 */}
-      <div className="candidatura" id="candidatura1">
-        <h2>CANDIDATURA 1</h2>
-        <form onSubmit={sendVoting}>
-          <div className="option">
-            {voting.question.options.map((o) => (
-              <div className="card-input" key={o.number}>
-                <label>
-                  <input
-                    type="radio"
-                    name="product"
-                    className="card-input-element"
-                    onChange={(e) => setSelectedAnswer(o.number)}
-                    checked={selectedAnswer === o.number}
-                  />
-                  <div className="flip-card">
-                    <div className="flip-card-inner">
-                      <div className="flip-card-front">
-                        <h1>Candidato: {o.option}</h1>
-                        {/* <img
-                          src="img_avatar.png"
-                          alt="Avatar"
-                          style="width:300px;height:300px;"
-                        >
-                        </img> */}
-                      </div>
-                      <div className="flip-card-back">
-                        <h1>Candidato 1</h1>
-                        <p>Algo del candidato</p>
-                        <p>{o.option}</p>
-                        <p>Has elegido el candidato: {selectedAnswer}</p>
-                      </div>
-                    </div>
-                  </div>
-                </label>
-                <br />
-              </div>
-            ))}
-          </div>
-          <button>Vote</button>
-        </form>
-      </div>
-      {/* CANDIDATURA 2 */}
-
-      <div className="candidatura" id="candidatura2">
-        <h2>CANDIDATURA 2</h2>
-        <form onSubmit={sendVoting}>
-          <div className="option">
-            {voting.question.options.map((o) => (
-              <div className="card-input" key={o.number}>
-                <label>
-                  <input
-                    type="radio"
-                    name="product"
-                    className="card-input-element"
-                    onChange={(e) => setSelectedAnswer(o.number)}
-                    checked={selectedAnswer === o.number}
-                  />
-                  <div className="flip-card">
-                    <div className="flip-card-inner">
-                      <div className="flip-card-front">
-                        <h1>Candidato: {o.option}</h1>
-                        {/* <img
-                          src="img_avatar.png"
-                          alt="Avatar"
-                          style="width:300px;height:300px;"
-                        >
-                        </img> */}
-                      </div>
-                      <div className="flip-card-back">
-                        <h1>Candidato 1</h1>
-                        <p>Algo del candidato</p>
-                        <p>{o.option}</p>
-                        <p>Has elegido el candidato: {selectedAnswer}</p>
-                      </div>
-                    </div>
-                  </div>
-                </label>
-                <br />
-              </div>
-            ))}
-          </div>
-          <button>Vote</button>
-        </form>
-      </div>
-      {/* CANDIDATURA 3 */}
-
-      {/* <div className="" id="candidatura3">
-      <h2>CANDIDATURA 3</h2>
-
-        <form onSubmit={sendVoting}>
-          <div className="row">
-            {voting.question.options.map((o) => (
-              <div key={o.number}>
-                <label>
-                  <input
-                    type="radio"
-                    name="product"
-                    className="card-input-element"
-                    onChange={(e) => setSelectedAnswer(o.number)}
-                    checked={selectedAnswer === o.number}
-                  />
-                  <div className="flip-card">
-                    <div className="flip-card-inner">
-                      <div className="flip-card-front">
-                        <h1>Candidato: {o.option}</h1>
-                        
-                      </div>
-                      <div className="flip-card-back">
-                        <h1>Candidato 1</h1>
-                        <p>Algo del candidato</p>
-                        <p>{o.option}</p>
-                        <p>Has elegido el candidato: {selectedAnswer}</p>
-                      </div>
-                    </div>
-                  </div>
-                </label>
-                <br />
-              </div>
-            ))}
-          </div>
-          <button>Vote</button>
-        </form>
+    <div id="content" className="voting">
+      {/* <div>
+        <button id="prev-question">Prev Question </button>
+        <button id="next-question">Next Question </button>
       </div> */}
+      <form onSubmit={sendVoting}>
+        {voting.question.map((o) => (
+          <div className="question">
+            <h2>{o.desc}</h2>
+            {o.options.map((p) => (
+              <div>
+                <div className="option">
+                  <div className="card-input" key={o.number}>
+                    <label>
+                      {/* <input
+                        type="radio"
+                        name="product"
+                        className="card-input-element"
+                        onChange={(e) => setSelectedAnswer(o.number)}
+                        checked={selectedAnswer === o.number}
+                      /> */}
+                      <input
+                        type="radio"
+                        name={o.desc}
+                        className="card-input-element"
+                        value={p.number}
+                        required
+                      />
+                      {p.option}
+                      <div className="flip-card">
+                        <div className="flip-card-inner">
+                          <div className="flip-card-front">
+                            <h1>Candidato: {o.option}</h1>
+                          </div>
+                          <div className="flip-card-back">
+                            <h1>Candidato 1</h1>
+                            <p>Algo del candidato</p>
+                            <p>{o.option}</p>
+                            <p>Has elegido el candidato: {selectedAnswer}</p>
+                          </div>
+                        </div>
+                      </div>
+                    </label>
+                    <br />
+                  </div>
+                </div>
+                <br />
+              </div>
+            ))}
+          </div>
+        ))}
+
+        <button>Vote</button>
+      </form>
     </div>
   );
 };
 export default Voting;
 
-
+{
+  /* onChange={e => setObjeto(...objeto,{[o.desc]:p.number})}
+<h2>{voting.question.desc}</h2>
+            <form onSubmit={sendVoting}>
+                {voting.question.options.map(o => (
+                    <div key={o.number}>
+                        <input type="radio" onChange={e => setSelectedAnswer(o.number)} checked={selectedAnswer === o.number} />
+                        {o.option}
+                        <br />
+                    </div>
+                ))}
+                <button>Vote</button>
+            </form>
             {o.options.map(p => (
                         <div key={p.number}>
                             <input type="radio" onChange={e => setSelectedAnswer(p.number)} checked={selectedAnswer === p.number} />
                             {p.option}
                             <br />
                         </div>
-                    ))}*/
+                    ))} */
+}
+{
+  /* <img
+                          src="img_avatar.png"
+                          alt="Avatar"
+                          style="width:300px;height:300px;"
+                        >
+                        </img> */
+}
