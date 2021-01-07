a0d5f7af185a957a93334075c4bddac9daad5e95
miguelponce82 <migponmel@alum.us.es>
2021-01-07 13:15:33 +0100

    TestFiles 1

---
+++ loadtest/gen_voting.py
@@ -0,0 +1,54 @@
+import json
+import requests
+
+
+HOST = "http://localhost:8000"
+USER = "decide"
+PASS = "decide"
+
+
+
+def create_users(filename):
+    """
+    Create voters with requests library from filename.json, where key are
+    usernames and values are the passwords.
+    """
+    with open(filename) as f:
+        voters = json.loads(f.read())
+
+    data = {'username': USER, 'password': PASS}
+    response = requests.post(HOST + '/authentication/login/', data=data)
+    token = response.json()
+
+    voters_pk = []
+    invalid_voters = []
+    for username, pwd in voters.items():
+        token.update({'username': username, 'password': pwd})
+        response = requests.post(HOST + '/authentication/register/', data=token)
+        if response.status_code == 201:
+            voters_pk.append(response.json().get('user_pk'))
+        else:
+            invalid_voters.append(username)
+    return voters_pk, invalid_voters
+    
+def create_voters(filename):
+    """
+    make all the users VotingUsers
+    """
+    with open(filename) as f:
+    voters = json.loads(f.read())
+    data = {'username': USER, 'password': PASS}
+    response = requests.post(HOST + 'admin/authentication/votinguser/add/', data=data)
+    token = response.json()
+    
+
+
+
+
+
+
+
+
+
+voters, invalids = create_users('voters.json')
+print("Create voters with pk={0} \nInvalid usernames={1}".format(voters, invalids))
